e599f250190b5db18e0319c880e2ab551a20a245
Jackutea <chenwansal1@163.com>
2024-05-10 20:39:15 +0800

    ESTABLISH

---
+++ Assets/Scripts/Main.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace SaveLoadTutorial {
+
+    public class Main : MonoBehaviour {
+
+        void Start() {
+            Debug.Log("Hello");
+        }
+
+        void Update() {
+
+        }
+
+    }
+}
